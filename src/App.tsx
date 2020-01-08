71b24d011b084375161148a906544b59389de923
Silas Kenneth <silaskenneth1@gmail.com>
2020-01-08 15:45:23 +0300

    Finish fragments

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component
+{
+  state = {
+    developers: []
+  }
+
+   componentDidMount()
+   {
+     axios.get("https://api.github.com/users")
+       .then((response) => {
+         const {data} = response;
+         this.setState({developers: data});
+       })
+     this.setState({loaded: "Yes"});
+   }
+   createList(developer: any): React.ReactNode
+   {
+     return <React.Fragment key={developer.login}>
+       <td>{developer.login}</td>
+       <td>{developer.id}</td>
+       <td><img src={developer.avatar_url} /></td>
+       <td> </td>
+     </React.Fragment>
+   }
+   render(): React.ReactNode
+   {
+     const {developers} = this.state;
+     console.log(this.state);
+     return (
+       <React.Fragment>
+    <header>
+      <h1>Hello world</h1>
+    </header>
+    <div className='container'>
+      <table>
+         <tbody>
+         {developers.slice(0, 10).map((developer: any | object) => {
+           return <tr key={developer.login}>
+             {this.createList(developer)}
+           </tr>
+         })}
+         </tbody>
+      </table>
     </div>
+    </React.Fragment>
   );
+   }
 }
 
 export default App;
